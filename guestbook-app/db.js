3bb531d98ef85aa1ed8f645b531a1d4d39798d66
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2021-10-30 10:04:29 +0200

    add missing JS

---
+++ guestbook-app/db.js
@@ -0,0 +1,91 @@
+var cfg = require('nconf');
+var mysql = require('mysql');
+
+// Load the AWS SDK
+var AWS = require('aws-sdk');
+
+// Retrieve region and secret name from environment 
+var region = process.env.GUESTBOOK_REGION || "eu-central-1";
+var secretName = process.env.GUESTBOOK_SECRET_NAME || "guestbook-dev-master-credentials";
+console.log('region:', region);
+console.log('secretName:', secretName);
+
+var secret = null;
+var decodedBinarySecret = null;
+
+// Create a Secrets Manager client
+var client = new AWS.SecretsManager({
+    region: region
+});
+
+var connection;
+
+// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+// We rethrow the exception by default.
+
+client.getSecretValue({SecretId: secretName}, function(err, data) {
+    if (err) {
+        throw err;
+    }
+    else {
+        // Decrypts secret using the associated KMS CMK.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if ('SecretString' in data) {
+            secret = data.SecretString;
+        } else {
+            let buff = new Buffer(data.SecretBinary, 'base64');
+            decodedBinarySecret = buff.toString('ascii');
+        }
+    }
+
+    var secretJSON = JSON.parse(secret);
+    console.log('getting secret', secretJSON);
+    var db_config = {
+        host: secretJSON.host || (process.env.db_info && process.env.db_info.host) || cfg.get('db:host'),
+        user: secretJSON.username || (process.env.db_info && process.env.db_info.username) || cfg.get('db:username'),
+        password: secretJSON.password || (process.env.db_info && process.env.db_info.password) || cfg.get('db:password'),
+        database: secretJSON.database || (process.env.db_info && process.env.db_info.database) || cfg.get('db:database')
+    };
+
+    function handleDisconnect() {
+        connection = mysql.createConnection(db_config);
+
+        connection.connect(function(err) {
+            if (err) {
+                console.log('error when connecting to db:', err);
+                setTimeout(handleDisconnect, 2000);
+            }
+        });
+
+        connection.on('error', function(err) {
+            console.log('db error', err);
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                handleDisconnect();
+            } else {
+                throw err;
+            }
+        });
+    }
+
+    handleDisconnect();
+});
+
+
+module.exports = {
+
+    // Return all post made to the guestbook
+    getGuestbook: function(callback) {
+        connection.query('SELECT * FROM `guestbook` ORDER BY `date` DESC;', function(err, rows, fields) {
+            callback(err, rows);
+        });
+    },
+
+    // Insert a new comment to the guestbook
+    setGuestbook: function(name, message, callback) {
+        connection.query('INSERT INTO `guestbook`(`name`, `message`) VALUE(?, ?);',
+            [name, message], function(err, rows, fields) {
+            callback(err, rows);
+        });
+    }
+};
---
+++ guestbook-cdk/lib/custom-resource/database-setup/index.js
@@ -0,0 +1,99 @@
+exports.main = function(event, context, callback) {           
+    // Load the AWS SDK
+    var AWS = require('aws-sdk');
+    const region = process.env.AWS_REGION;
+    var mysql = require('mysql');
+    var cfnresponse = require('cfn-response');
+    var secret = null;
+    var decodedBinarySecret = null;
+
+    console.log('Starting Lambda using region: ' + region);
+    console.log('Event received', event);
+    console.log('Environment [secretName] :', process.env.secretName)
+    
+    // Only execute following code on create (do nothing in other cases)
+    if (event.RequestType != 'Create') {
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {"Data": "ok"});        
+        return;
+    }
+    
+    // Create a Secrets Manager client
+    var client = new AWS.SecretsManager({
+        region: region
+    });
+    
+    // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    // We rethrow the exception by default.
+    
+    client.getSecretValue({SecretId: process.env.secretName}, function(err, data) {
+        if (err) {
+            if (err.code === 'DecryptionFailureException')
+                // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw err;
+            else if (err.code === 'InternalServiceErrorException')
+                // An error occurred on the server side.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw err;
+            else if (err.code === 'InvalidParameterException')
+                // You provided an invalid value for a parameter.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw err;
+            else if (err.code === 'InvalidRequestException')
+                // You provided a parameter value that is not valid for the current state of the resource.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw err;
+            else if (err.code === 'ResourceNotFoundException')
+                // We can't find the resource that you asked for.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                throw err;
+        }
+        else {
+            // Decrypts secret using the associated KMS CMK.
+            // Depending on whether the secret is a string or binary, one of these fields will be populated.
+            if ('SecretString' in data) {
+                secret = data.SecretString;
+            } else {
+                let buff = new Buffer(data.SecretBinary, 'base64');
+                decodedBinarySecret = buff.toString('ascii');
+            }
+        }
+        
+        var secretJson = JSON.parse(secret);
+        var mysql = require('mysql');
+        var con = mysql.createConnection({
+          host: secretJson.host,
+          user: secretJson.username,
+          password: secretJson.password,
+          database: 'mysql',
+          multipleStatements: true
+        });
+        
+        con.connect(function(err) {
+          if (err) {
+              console.log('Error occured while connecting', err);
+              cfnresponse.send(event, context, cfnresponse.FAILED, {"Data": "notOk"});
+              return;
+          }
+          
+          // @todo : Move sql outside
+          con.query("CREATE DATABASE IF NOT EXISTS " + secretJson.database +"; CREATE TABLE IF NOT EXISTS " + secretJson.database + ".guestbook ( \
+  `id` INT NOT NULL AUTO_INCREMENT, \
+  `name` VARCHAR(32) NOT NULL, \
+  `message` TEXT NULL, \
+  `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+  PRIMARY KEY (`id`));", function (err, result, fields) {
+            if (err) {
+                console.log('Error - couldnt create schema', err);
+                cfnresponse.send(event, context, cfnresponse.FAILED, {"Data": "notOk"});                
+                return;
+            }
+            console.log('success', result);
+            con.end();
+            cfnresponse.send(event, context, cfnresponse.SUCCESS, {"Data": "ok"});            
+            return;
+          });
+        });  
+    });
+}
